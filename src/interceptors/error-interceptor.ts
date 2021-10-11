4e1c79bd4833491bc9b669e441c7d37ede112687
Rafael de Souza Alves <rafael-souza4@outlook.com>
2021-10-11 11:16:50 -0300

    Criando Pagina de produtos

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -97,7 +97,7 @@ export class ErrorInterceptor implements HttpInterceptor {
     }
 
     listErrors(messages: FieldMessage[]) : string {
-        let s : string = '';       
+        let s : string = '';           
         for (var i=0; i<messages.length; i++) {
             s = s + "<p><strong>" + messages[i].fieldName + "</strong>: " + messages[i].message + "</p>";
         }
