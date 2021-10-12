7bdf9cc1dbadacf7a6d90ed2ecd2da723a668c4d
Rafael de Souza Alves <rafael-souza4@outlook.com>
2021-10-12 15:20:37 -0300

    buscando enderecos do banco de dados

--- src/pages/pick-address/pick-address.ts
+++ src/pages/pick-address/pick-address.ts
@@ -1,3 +1,5 @@
+import { ClienteService } from './../../services/domain/cliente.service';
+import { StorageService } from './../../services/storage.service';
 import { EnderecoDTO } from './../../models/endereco.dto';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
@@ -11,28 +13,28 @@ export class PickAddressPage {
 
   items: EnderecoDTO[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    public storage: StorageService, public clienteService: ClienteService) {
   }
 
   ionViewDidLoad() {
-    this.items = [
-      {
-        id: "1",
-        logradouro: "Rua Dr. Manoel Moreira",
-        numero: "300",
-        complemento: "",
-        bairro: "Centro",
-        cep: "151615",
-        cidade: {
-          id: "1",
-          nome: "Unberlandia",
-          estado: {
-            id: "1",
-            nome: "Minas Gerais"          
-          }
-        }
-      }
-    ]
+
+    let localUser = this.storage.getLocalUser();
+    if (localUser && localUser.email) {
+      this.clienteService.findByEmail(localUser.email)
+        .subscribe(response => {
+          this.items = response['enderecos'];          
+        },
+          error => {
+            if (error.status == 403) {
+              this.navCtrl.setRoot('HomePage');
+            }
+          });
+    }
+    else {
+      this.navCtrl.setRoot('HomePage');
+    }
+    
   }
 
 }
