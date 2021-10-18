7390c67782466383cb8f56908e2b07426b43c83e
Rafael de Souza Alves <rafael-souza4@outlook.com>
2021-10-18 07:34:09 -0300

    Enviando API para teste de envio de email

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080",
+    baseUrl: "https://loja-spring-ionic.herokuapp.com",
     bucketBaseUrl: "https://api-loja-ionic.s3.sa-east-1.amazonaws.com"
 };
